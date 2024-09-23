2301acb3f89e27b00bf8c2be46c901e919abaad9
aibiansari <aibiansari1@gmail.com>
2024-09-23 21:47:17 +0500

    theme icons fill on select

--- src/components/ui/dropdown.tsx
+++ src/components/ui/dropdown.tsx
@@ -46,7 +46,7 @@ const itemVariants = {
 
 const dropdownItems = [
   {
-    icon: (
+    icon: () => (
       <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
         <path d="M8 15A7 7 0 1 0 8 1zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16" />
       </svg>
@@ -55,18 +55,25 @@ const dropdownItems = [
     theme: "system",
   },
   {
-    icon: (
+    icon: (isSelected: boolean) => (
       <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
         <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6m0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708" />
+        {isSelected ? <circle cx="8" cy="8" r="3.4" /> : null}
       </svg>
     ),
     label: "Light",
     theme: "light",
   },
   {
-    icon: (
+    icon: (isSelected: boolean) => (
       <svg fill="currentColor" className="w-5 h-5" viewBox="0 0 16 16">
         <path d="M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278M4.858 1.311A7.27 7.27 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.32 7.32 0 0 0 5.205-2.162q-.506.063-1.029.063c-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286" />
+        {isSelected ? (
+          <path
+            d="M6.3,15.3c0,0-5.8-1.3-5.8-7.1s4.4-7.3,4.4-7.3l0.6-0.2c0,0-2.2,4.7,0.6,8.5s8.1,3.4,8.2,3.2C14.5,12.3,12.2,16.6,6.3,15.3z
+	"
+          />
+        ) : null}
       </svg>
     ),
     label: "Dark",
@@ -157,20 +164,20 @@ const DropDown = () => {
 
         <motion.ul
           variants={itemVariants}
-          className="flex items-center justify-center gap-2 p-2"
+          className="flex items-center justify-center gap-2 p-1.5"
         >
           {dropdownItems.map((item, index) => (
             <motion.li
               key={index}
               onClick={() => setTheme(item.theme)}
-              className={`flex py-1 text-[10px] font-medium cursor-pointer flex-col w-12 ring-1 ring-neutral-400 dark:ring-neutral-900 rounded-md items-center justify-center space-y-1.5 transition-colors duration-500 ease-out ${
+              className={`flex pt-1.5 pb-1 text-[10px] font-medium cursor-pointer flex-col w-12 ring-1 ring-neutral-400 dark:ring-neutral-900 rounded-md items-center justify-center space-y-1.5 transition-colors duration-500 ease-out ${
                 theme === item.theme
                   ? "bg-neutral-400 dark:bg-neutral-700"
                   : "hover:bg-neutral-300 dark:hover:bg-element"
               }`}
               variants={itemVariants}
             >
-              {item.icon}
+              {item.icon(theme === item.theme)}
               <span>{item.label}</span>
             </motion.li>
           ))}
