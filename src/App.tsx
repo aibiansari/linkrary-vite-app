30f062553f6fbdb1748889bf4db8d82a8d74f10c
aibiansari <aibiansari1@gmail.com>
2024-10-01 21:50:05 +0500

    Gh-pages Fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { FilterButtonProvider } from "./contexts/FilterButtonContext";
 import { FavoriteCardsProvider } from "./contexts/FavoriteCardsContext";
 import { CategoryModalProvider } from "./contexts/CategoryModalContext";
