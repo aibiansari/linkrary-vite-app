efa7d2934005f54fd57225b7c340bf5ca3b4a876
aibiansari <aibiansari1@gmail.com>
2024-10-03 11:10:35 +0500

    Linkrary in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Linkrary/", //here goes your own repository names
+  base: "/linkrary-vite-app/", //here goes your own repository names
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
